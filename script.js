8fe9bf3882ff70180b06087715ad3d840fed55ec
Roshan <71066704+roshan194@users.noreply.github.com>
2025-04-01 13:40:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
-//complete this code
-class Person {}
+// Base class: Person
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-class Student extends Person {}
+  // Getter for name
+  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  // Setter for age
+  set age(newAge) {
+    if (newAge > 0) {
+      this._age = newAge;
+    } else {
+      console.log("Age must be a positive number.");
+    }
+  }
+}
+
+// Subclass: Student (inherits from Person)
+class Student extends Person {
+  study() {
+    console.log(`${this.name} is studying`);
+  }
+}
+
+// Subclass: Teacher (inherits from Person)
+class Teacher extends Person {
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
+}
 
 // Do not change the code below this line
 window.Person = Person;
